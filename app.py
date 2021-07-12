fb1e9631f746363e242ff5536c15c3d487717d9e
clement <clementcamara@gmail.com>
2021-07-12 12:26:44 +0200

    te

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("Hello guys")
 
 
-data = pd.read_csv('spam.csv', sep=',', skip_blank_lines=True, a_filter=True)
+data = pd.read_csv('spam.csv', sep=',', skip_blank_lines=True)
 
 
 st.dataframe(data.head())
