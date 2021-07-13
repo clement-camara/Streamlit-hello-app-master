946b28b2e321b082ac3dd20e4047af6c8bf7670b
clement <clementcamara@gmail.com>
2021-07-13 14:54:07 +0200

    fast api

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import pandas as pd
+import requests
 
-st.title("Hello guys")
+st.title("NLP Project")
+
+url = 'http://127.0.0.1:8000/'
+endpoint = '/'
+
+st.write('''
+         Visit this URL at `http://127.0.0.1:8000/` for FastAPI documentation.
+         ''')
+
+#data = pd.read_csv('spam.csv', sep=',', skip_blank_lines=True)
 
 
-data = pd.read_csv('spam.csv', sep=',', skip_blank_lines=True)
-
-
-st.dataframe(data.head())
+#st.dataframe(data.head())
